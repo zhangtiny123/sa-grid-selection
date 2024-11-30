e9a3572069eac53ad7aeaf1d14ef7f85fd04ea0c
Thomas <tzhang920314@gmail.com>
2024-11-30 14:43:26 -0500

    chore: define new colors and new font family Europa

--- src/designSystem.js
+++ src/designSystem.js
@@ -1,6 +1,8 @@
 'use strict'
 
 import ICONS from '@symbo.ls/default-icons'
+import europaRegular from './fonts/europa-regular-webfont.woff2'
+import europaBold from './fonts/europa-bold-webfont.woff2'
 
 /**
  * Colors
@@ -10,7 +12,10 @@ const COLOR = {
   transparent: 'transparent',
   black: 'black',
   white: 'white',
-  orange: '#E98232'
+  orange: '#E98232',
+  gray: 'rgba(0, 0, 0, 0.5)',
+  cellActive: '#3D7BD9',
+  cellInactive: '#E8F1FF'
 }
 
 /**
@@ -44,13 +49,31 @@ const THEME = {
  * Fonts
  * @tutorial https://docs.symbols.app/font
  */
-const FONT = {}
+const FONT = {
+  Europa: [{
+    url: europaRegular,
+    fontWeight: 400,
+    fontStyle: 'normal',
+    fontDisplay: 'swap'
+  }, {
+    url: europaBold,
+    fontWeight: 700,
+    fontStyle: 'normal',
+    fontDisplay: 'swap'
+  }]
+}
 
 /**
  * Font families
  * @tutorial https://docs.symbols.app/font
  */
-const FONT_FAMILY = {}
+const FONT_FAMILY = {
+  EuropaFonts: {
+    isDefault: true,
+    value: ['Europa'],
+    type: 'sans-serif'
+  }
+}
 
 /**
  * Typography
